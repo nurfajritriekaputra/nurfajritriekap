8b8acada51771be133009a2414947ea7af38b41f
nurfajritriekaputra <47167504+nurfajritriekaputra@users.noreply.github.com>
2019-01-30 13:36:21 +0800

    Update perkenalan.php

--- perkenalan.php
+++ perkenalan.php
@@ -1,6 +1,6 @@
 <table width="350">
 
-<td width="300" height="3315" align="left"> <p><strong> Nama : Nurfajri Trieka Putra</strong></p>
+<td width="300" height="315" align="left"> <p><strong> Nama : Nurfajri Trieka Putra</strong></p>
   <p><strong> Nim : 1215 280 224 </strong></p>
   <p><strong> Telephone : 081340211*** </strong></p>
   <p><strong> Email : nurfajritriekap@gmail.com </strong></p>

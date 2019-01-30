46b51d0d982fa27c889073c22a7ecc23f6512181
nurfajritriekaputra <47167504+nurfajritriekaputra@users.noreply.github.com>
2019-01-30 12:06:52 +0800

    Create perkenalan.php

---
+++ perkenalan.php
@@ -0,0 +1,8 @@
+<table width="337">
+
+<td width="298" height="300" align="left"> <p><strong> Nama : Nurfajri Trieka Putra</strong></p>
+  <p><strong> Nim : 1215 280 224 </strong></p>
+  <p><strong> Telephone : 081340211*** </strong></p>
+  <p><strong> Email : nurfajritriekap@gmail.com </strong></p>
+  <p><strong> Alamat : Sidenreng Rappang </strong></p></td>
+</table>
